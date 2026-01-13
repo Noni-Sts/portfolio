959fcf263351c7da369345fa2b9acb6281a8756d
Noni Santos <nonhlanhlasantos@gmail.com>
2026-01-13 18:23:25 +0200

    Folders, scaffolds and set up fixes

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+function App() {
+  return (
+    <div>
+      <h1>Starting Soon</h1>
+    </div>
+  );
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default App;
