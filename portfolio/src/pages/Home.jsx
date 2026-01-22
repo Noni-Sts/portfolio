c9c7ca679c9c87e6dd4af483a2211bfa15f897ab
Noni Santos <nonhlanhlasantos@gmail.com>
2026-01-22 20:09:03 +0200

    Hero Scaffold

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,8 +1,9 @@
+import Hero from "../sections/Hero";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Main Site goes here</h1>
-      <p>This will be your portfolio layout / sections / etc.</p>
-    </div>
+    <main>
+      <Hero />
+    </main>
   );
 }
---
+++ portfolio/src/sections/Hero.jsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <section>
+      <p>Hi, I’m</p>
+      <h1>Noni</h1>
+
+      <p>
+        I build clean, responsive web apps and I’m transitioning into software
+        development.
+      </p>
+
+      <div>
+        <a href="#projects">View Projects</a>
+        <a href="#contact">Contact Me</a>
+      </div>
+    </section>
+  );
+}
