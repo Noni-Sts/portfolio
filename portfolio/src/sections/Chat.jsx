e614d9f4fe7ff7156fc7e26ea8c40c3fd16871e1
Noni Santos <nonhlanhlasantos@gmail.com>
2026-05-17 11:49:17 +0200

    feat: animate live chat bubbles

--- portfolio/src/sections/Chat.jsx
+++ portfolio/src/sections/Chat.jsx
@@ -1,21 +1,40 @@
 import ChatBubble from "../components/ChatBubble";
 import "../styles/Chat.css";
 
+const messages = [
+  "👋 Hey, I’m Noni",
+  "💻 self-taught dev + bootcamp grad",
+  "🌸 portfolio in progress…",
+  "🎮 currently building fun UI ideas",
+  "☕ powered by tea and panic",
+];
+
 export default function Chat() {
   return (
     <aside className="chat">
+      {/* CHAT MESSAGES */}
       <div className="chat__messages">
-        <ChatBubble text="👋 Hey, I’m Noni" />
-        <ChatBubble text="💻 self-taught dev + bootcamp grad" />
-        <ChatBubble text="🌸 portfolio in progress…" />
+        {messages.map((msg, index) => (
+          <div
+            className="chat__message-wrap"
+            key={msg}
+            style={{
+              animationDelay: `${index * 1.2}s`,
+            }}
+          >
+            <ChatBubble text={msg} />
+          </div>
+        ))}
       </div>
 
+      {/* INPUT BAR */}
       <div className="chat__input">
         <span className="typing-dots">
           <i></i>
           <i></i>
           <i></i>
         </span>
+
         <input type="text" placeholder="Chat opens soon…" disabled />
       </div>
     </aside>
