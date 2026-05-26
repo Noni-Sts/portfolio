94eb5df12c7a1043877170b1922098012c7a7a2d
Noni Santos <nonhlanhlasantos@gmail.com>
2026-05-26 19:53:06 +0200

    refactor: chatbubble divs

--- portfolio/src/components/ChatBubble.jsx
+++ portfolio/src/components/ChatBubble.jsx
@@ -1,3 +1,11 @@
-export default function ChatBubble({ text }) {
-  return <div className="chat-bubble chat-bubble--system">{text}</div>;
+export default function ChatBubble({ user, text }) {
+  return (
+    <div className="chat-bubble">
+      {/* Fake username */}
+      <span className="chat-bubble__user">{user}</span>
+
+      {/* Actual message */}
+      <div className="chat-bubble__text">{text}</div>
+    </div>
+  );
 }
