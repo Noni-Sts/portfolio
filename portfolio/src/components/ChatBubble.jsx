5e4b64314586d868868ba83b949759410f227586
Noni Santos <nonhlanhlasantos@gmail.com>
2026-05-26 20:13:50 +0200

    refactor: chat bubble styling

--- portfolio/src/components/ChatBubble.jsx
+++ portfolio/src/components/ChatBubble.jsx
@@ -1,10 +1,30 @@
-export default function ChatBubble({ user, text }) {
+export default function ChatBubble({ user, text, type, index }) {
+  /*
+    Alternates slight left/right positioning
+    so chat feels less rigid
+  */
+
+  const offsetClass =
+    index % 2 === 0 ? "chat-bubble--left" : "chat-bubble--right";
+
+  /*
+    MOD messages use special styling
+  */
+
+  if (type === "mod") {
+    return (
+      <div className="chat-bubble chat-bubble--mod">
+        <span className="chat-bubble__mod-text">{text}</span>
+      </div>
+    );
+  }
+
   return (
-    <div className="chat-bubble">
-      {/* Fake username */}
+    <div className={`chat-bubble ${offsetClass}`}>
+      {/* USERNAME */}
       <span className="chat-bubble__user">{user}</span>
 
-      {/* Actual message */}
+      {/* MESSAGE */}
       <div className="chat-bubble__text">{text}</div>
     </div>
   );
